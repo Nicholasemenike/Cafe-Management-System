7e577ef2e3ae1aba7f8d143e227506a1bea5475b
NicholasEmenike <emenikenicholas022@gmail.com>
2023-10-28 18:06:09 +0100

    first commit

---
+++ src/main/java/com/system/cafe/Cafe_system/CafeManagementSystemProjectApplication.java
@@ -0,0 +1,13 @@
+package com.system.cafe.Cafe_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeManagementSystemProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeManagementSystemProjectApplication.class, args);
+	}
+
+}
